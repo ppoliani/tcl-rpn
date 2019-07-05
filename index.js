25ea9b00f4d55432996c53a7e269e5c670a678cc
ppoliani <ppoliani@gmail.com>
2019-07-05 09:56:53 +0100

    implement postfix to infix

--- index.js
+++ index.js
@@ -34,39 +34,42 @@ const processOperator = (operators, stack, result, token) => {
   let topItem = peek(stack);
 
   while (isOperator(operators, topItem) && getPrecedence(operators, topItem) >= getPrecedence(operators, token)) {
-    result = [...result, stack.pop()];
+    result = push(result, pop(stack));
     topItem = peek(stack);
   }
 
   return {
-    stack: [...stack, token],
+    stack: push(stack, token),
     result
   }
 }
 
-const processOperand = (result, token) => [...result, token];;
+const processOperand = (result, token) => push(result, token);
 
-const processOpeningParenthesis = (stack, token) => [...stack, token];
+const processOpeningParenthesis = (stack, token) => push(stack, token);
 
 const processClosingParenthesis = (stack, result) => {
   while (peek(stack) !== '(') {
-    result = [...result, stack.pop()];
+    result = push(result, pop(stack));
   }
 
-  stack.pop();
+  pop(stack);
 
   return {stack, result};
 }
 
+const pop = stack => stack.pop();
+const push = (stack, val) => [...stack, val];
+
 const reduceExpression = (stack, result) => {
   while (stack.length) {
-    result = [...result, stack.pop()]
+    result = [...result, pop(stack)]
   }
 
   return result.join(' ');
 }
 
-const rpn = (expression, options={}) => {
+const postfix = (expression, options={}) => {
   const {operators =  [
     {name: 'AND', precedence: 1},
     {name: 'OR', precedence: 1}
@@ -85,4 +88,32 @@ const rpn = (expression, options={}) => {
   return reduceExpression(stack, result);
 }
 
-module.exports = {rpn};
+const infix = (expression, options={}) => {
+  const {operators =  [
+    {name: 'AND', precedence: 1},
+    {name: 'OR', precedence: 1}
+  ]} = options;
+
+  let stack = [];
+
+  tokenize(expression).forEach(token => {
+    if(!isOperator(operators, token)) {
+      stack = push(stack, token);
+    }
+    else {
+      const topItem = pop(stack);
+      const pushVal = `(${pop(stack)} ${token} ${topItem})`;
+      stack = push(stack, pushVal);
+    }
+  });
+
+  return peek(stack);
+}
+
+console.log(postfix('A AND B OR ( C AND D )')) // A B AND C D AND OR postfix
+console.log(infix('A B AND C D AND OR')) // A AND B OR ( C AND D ) infix
+
+module.exports = {
+  postfix,
+  infix
+};
